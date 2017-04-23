aced5d2ffd2067e79de2d36ad1bba9dec6439d38
Antonio Revert Hinojosa <a.revert.hinojosa@gmail.com>
2017-04-23 18:24:24 +0200

    Creada vista de registre d'usuari, i fixters Angular per a que funcione

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -1,2 +1,34 @@
-angular
-  .module('thinkster', []);
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.config', []);
+
+  angular
+    .module('thinkster', [
+      'thinkster.routes',
+      'thinkster.authentication',
+      'thinkster.config'
+    ]);
+
+  angular
+    .module('thinkster.routes', ['ngRoute']);
+
+
+  /*Codi per a donar suport al CSRF token de django:*/
+  angular
+  .module('thinkster')
+  .run(run);
+
+  run.$inject = ['$http'];
+
+  /**
+  * @name run
+  * @desc Update xsrf $http headers to align with Django's defaults
+  */
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
+
+})();
